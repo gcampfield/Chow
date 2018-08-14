03f69316e9d423f351c2e48409100d6a7ef6bb82
Grant Campfield <grant@gcampfield.com>
2018-08-13 18:35:11 -0700

    Define swipe directions

---
+++ Chow/SwipeView/SwipeDirection.swift
@@ -0,0 +1,44 @@
+//
+//  SwipeDirection.swift
+//  Chow
+//
+//  Created by Grant Campfield on 8/13/18.
+//  Copyright © 2018 Grant Campfield. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+enum SwipeDirection {
+    
+    case up
+    case right
+    case down
+    case left
+    
+    var point: CGPoint {
+        switch self {
+        case .up:    return CGPoint(x:  0, y:  1)
+        case .right: return CGPoint(x:  1, y:  0)
+        case .down:  return CGPoint(x: -1, y:  0)
+        case .left:  return CGPoint(x:  0, y: -1)
+        }
+    }
+    
+    static let all: [SwipeDirection] = [ .up, .right, .down, .left ]
+    
+    static func closestTo(_ point: CGPoint) -> SwipeDirection? {
+        return closestTo(point, withBuffer: 0.0)
+    }
+    
+    static func closestTo(_ point: CGPoint, withBuffer buffer: CGFloat) -> SwipeDirection? {
+        return all.reduce((distance: CGFloat.infinity, direction: nil), { closest, direction -> (CGFloat, SwipeDirection?) in
+            let distance = direction.point.distanceTo(point)
+            if distance < closest.distance && distance >= buffer {
+                return (distance, direction)
+            }
+            return closest
+        }).direction
+    }
+    
+}
