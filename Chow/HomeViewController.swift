d1ac1dc566628a1fe5aaaabe969ba94ebe51d504
Grant Campfield <grant@gcampfield.com>
2018-08-18 15:02:59 -0700

    Seperate SwipeView delegate from Controller

--- Chow/HomeViewController.swift
+++ Chow/HomeViewController.swift
@@ -8,69 +8,20 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, SwipeViewDelegate {
+class HomeViewController: UIViewController {
 
+    let restaurantViewSwipeDelegate: RestaurantViewSwipeDelegate = RestaurantViewSwipeDelegate()
     @IBOutlet weak var currentRestaurantView: RestaurantView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // attach view controller as delegate for SwipeView
-        currentRestaurantView.delegate = self
+        currentRestaurantView.delegate = restaurantViewSwipeDelegate
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    // MARK: - SwipeViewDelegate
-
-    var small: Bool = false
-    func didTap(view: SwipeView) {
-        // TODO: segue to detail view for current restaurant
-    }
-    
-    func didBeginSwipe(on view: SwipeView) {
-        // TODO (?): handle the start of a swipe
-    }
-    
-    func didChangeSwipe(on view: SwipeView, in direction: SwipeDirection?, with translation: CGPoint) {
-        view.transform = tilt(view: view, for: translation)
-
-        // TODO: overlay meaning onto the card based on the swipe
-    }
-    
-    func didEndSwipe(on view: SwipeView, in direction: SwipeDirection?) {
-        guard let direction = direction else {
-            UIView.animate(withDuration: 0.5,
-                           delay: 0.0,
-                           usingSpringWithDamping: 0.75,
-                           initialSpringVelocity: 1.0,
-                           options: [],
-                           animations: { view.transform = .identity },
-                           completion: nil)
-            return
-        }
-        // TODO: animate the movement of the card off of the screen
-        print("ended swipe \(direction)")
-
-        UIView.animate(withDuration: 0.5,
-                       delay: 0.0,
-                       usingSpringWithDamping: 0.75,
-                       initialSpringVelocity: 1.0,
-                       options: [],
-                       animations: { view.transform = .identity },
-                       completion: nil)
-
-        // TODO: remove the card and move onto the next one
-    }
-
-    // MARK: - Helper methods
-
-    private func tilt(view: UIView, for translation: CGPoint) -> CGAffineTransform {
-        let moved = CGAffineTransform(translationX: translation.x, y: translation.y)
-        let rotation = sin(translation.x / view.frame.width / 4.0)
-        return moved.rotated(by: rotation)
     }
 
 }
