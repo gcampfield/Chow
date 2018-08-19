2998a2cca6559c7477d01581693f5936550d7294
Grant Campfield <grant@gcampfield.com>
2018-08-18 19:16:23 -0700

    Add restaurants to populate views

--- Chow/HomeViewController.swift
+++ Chow/HomeViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, SwipeViewDelegate {
 
     @IBOutlet weak var shadowRestaurantView: RestaurantView!
+    var nextRestaurant: Restaurant? {
+        didSet {
+            shadowRestaurantView.fill(for: nextRestaurant)
+        }
+    }
+
     @IBOutlet weak var currentRestaurantView: RestaurantView!
+    var currentRestaurant: Restaurant? {
+        didSet {
+            currentRestaurantView.fill(for: currentRestaurant)
+        }
+    }
+
+    let restaurants = RestaurantDeck()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // attach view controller as delegate for SwipeView
-        currentRestaurantView.delegate = RestaurantViewSwipeDelegate(currentRestaurantView, shadowView: shadowRestaurantView)
+        currentRestaurantView.delegate = self
+        currentRestaurant = restaurants.pop()
+        nextRestaurant = restaurants.pop()
     }
 
     override func didReceiveMemoryWarning() {
